5e5ee712bb50eb617ace26b3c2ae5f7a82babe30
Your Name <geralddizon84@yahoo.com>
2023-05-03 17:04:44 +0800

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import {Task} from './Tasks';
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'April 27th at 5:25pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Dec 18th at 3:00am',
+        reminder: false,
+    }
+]
